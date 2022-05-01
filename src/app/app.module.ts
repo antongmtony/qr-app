e4ef07e11c3a8541cddd58d07379040391e2bcec
D4DOM\agonzalezm <antonio.gmalagon@gmail.com>
2022-05-01 17:06:18 +0200

    QR Generator works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { QRCodeModule } from 'angular2-qrcode';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    QRCodeModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BrowserTestingModule } from '@angular/platform-browser/testing';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  testing = 'testing';
+
+  name = 'Torcuato';
+  surname = 'Fernandez';
+  url = 'www.afflelou.es';
+  email = 'antonio.gmalagon@gmail.com';
+  phone = '697478285';
+  street = 'Calle del Bronce 30';
+  floor = '3D';
+  state = 'Madrid';
+  city = 'Madrid';
+  zipCode = '28045';
+  country = 'Spain';
+
+
+
+  img = null;
+
+  // eslint-disable-next-line max-len
+  qrData = `BEGIN:VCARD\nVERSION:3.0\nN:${this.surname};${this.name}\nFN:${this.name + ' ' + this.surname}\nURL:${this.url}\nEMAIL;TYPE=INTERNET:${this.email}\nTEL;TYPE=voice,work,pref:${this.phone}\nADR:;;${this.street}, ${this.floor};${this.state};${this.city};${this.zipCode};${this.country}\nEND:VCARD`;
+
 
   constructor() {}
 
